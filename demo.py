0c61a25e3cf18bd0f5b627635ab988e9c858a3fb
hemang21232 <hemang21232@gmail.com>
2022-03-04 11:45:45 +0530

    initial commit

--- demo.py
+++ demo.py
@@ -1,2 +1,3 @@
-s = [x for x in range(0,71) if x%7 == 0]
-a = [x for x in range(0,91) if x%9 == 0]
+a = [x for x in range(0,71) if x%7 == 0]
+b = [x for x in range(0,91) if x%9 == 0]
+c = [x for x in range(0,51) if x%5 == 0]
